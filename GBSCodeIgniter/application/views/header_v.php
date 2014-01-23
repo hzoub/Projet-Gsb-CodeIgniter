51fa544642ad2084ef74007656f535f1eff03eed
zoubert <zouberthanem@gmail.com>
2014-01-23 20:31:09 +0100

    Ajout du dossier images & style dans le dossier application.

--- GBSCodeIgniter/application/views/header_v.php
+++ GBSCodeIgniter/application/views/header_v.php
@@ -4,14 +4,14 @@
   <head>
     <title>Intranet du Laboratoire Galaxy-Swiss Bourdin</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<link href="<?php echo base_url();?>styles/styles.css" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url();?>application/styles/styles.css" rel="stylesheet" type="text/css" />
     <!-- base_url() = http://127.0.0.1/Projet-GSB-CodeIgniter/GBSCodeIgniter/ -->
-    <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url();?>images/favicon.ico" />
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url();?>application/images/favicon.ico" />
   </head>
 	<body>
 		<div id="page">
 			<div id="entete">
-				<img src="<?php echo base_url();?>images/logo.jpg" id="logoGSB" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
+				<img src="<?php echo base_url();?>application/images/logo.jpg" id="logoGSB" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
 				<!-- Affiche le titre s'il a etait définis dans le controleur -->
 				<h1><?php if(isset($titre)) echo $titre; ?></h1>
 			</div>
