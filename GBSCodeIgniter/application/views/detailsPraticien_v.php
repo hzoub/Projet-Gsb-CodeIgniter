8e4843bb7dea6bec7f2c9bd6ded45bec867cd250
zoubert <zouberthanem@gmail.com>
2014-02-15 18:06:21 +0100

    Details praticien

--- GBSCodeIgniter/application/views/detailsPraticien_v.php
+++ GBSCodeIgniter/application/views/detailsPraticien_v.php
@@ -1,10 +1,33 @@
 <?php
 	foreach($results as $recupLst){ 
 				  		
-		echo "Nom -> ".$recupLst->PRA_NOM."<br>".
-			 "Prenom -> ".$recupLst->PRA_PRENOM."<br/>".
-			 "Adresse -> ".$recupLst->PRA_ADRESSE."<br/>".
-			 "Code postal -> ".$recupLst->PRA_CP."<br/>".
-			 "Ville -> ".$recupLst->PRA_VILLE;
+		$nom     = $recupLst->PRA_NOM;
+		$prenom  = $recupLst->PRA_PRENOM;
+		$adresse = $recupLst->PRA_ADRESSE;
+		$cp      = $recupLst->PRA_CP;
+		$ville   = $recupLst->PRA_VILLE;
 	}
 ?>
+<table>
+
+	<tr align=center>
+
+		<td>Nom</td>
+		<td>Prenom</td>
+		<td>Adresse</td>
+		<td>Code postal</td>
+		<td>Ville</td>
+
+	</tr>
+
+	<tr align=center>
+
+		<td><?php echo $nom ?></td>
+		<td><?php echo $prenom ?></td>
+		<td><?php echo $adresse ?></td>
+		<td><?php echo $cp ?></td>
+		<td><?php echo $ville ?></td>
+
+	</tr>
+
+</table>	
